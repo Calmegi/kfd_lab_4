9ea9b33ea99cf49fc94d77a0ce309707337c45ba
Calmegi <116198893+Calmegi@users.noreply.github.com>
2025-03-17 13:52:21 +0300

    Add files via upload

--- src/main/kotlin/com/example/lab4/services/AdminRegistrator.kt
+++ src/main/kotlin/com/example/lab4/services/AdminRegistrator.kt
@@ -1,18 +1,12 @@
 package com.example.lab4.services
 
 import com.example.lab4.db.ExchangerUser
-//import com.example.lab4.db.repositories.UserRepository
 import com.example.lab4.repositories.UserRepository
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
-/*
-Сервис регистрирует администратора при запуске приложения.
-Если пользователь с username "admin" отсутствует, он создаётся с логином и паролем "admin".
-Также инициализируется стартовый баланс.
-*/
 
 @Service
 class AdminRegistrator(
@@ -29,7 +23,6 @@ class AdminRegistrator(
             password = passwordEncoder.encode("admin"),
             authorities = listOf("ROLE_USER", "ROLE_ADMIN")
         )
-        // Инициализация баланса для администратора.
         adminUser.balances.add(com.example.lab4.db.Balance("RUB", 1000000 * 100, adminUser))
         userRepository.save(adminUser)
     }
