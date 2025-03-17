ab6c2d4c43ef603312de219c59c6e2145aa2c0ab
Calmegi <116198893+Calmegi@users.noreply.github.com>
2025-03-17 13:49:59 +0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    // Kotlin плагин для JVM и Spring
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     kotlin("plugin.jpa") version "1.9.25"
-    // Плагин Spring Boot
     id("org.springframework.boot") version "3.4.0"
-    // Управление зависимостями Spring
     id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -15,7 +12,6 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    // Настройка toolchain для Java 17
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
@@ -26,25 +22,16 @@ repositories {
 }
 
 dependencies {
-    // Зависимости Spring Boot (валидация, JPA, Security, Web)
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-    // Jackson для Kotlin и рефлексия
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    // H2 база данных для хранения данных
     runtimeOnly("com.h2database:h2")
-
-    // Зависимости для тестирования
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
-
-    // JWT библиотека jjwt (API и реализации)
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
@@ -54,14 +41,12 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-// Настройка дополнительных опций компиляции для JSR305
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += listOf("-Xjsr305=strict")
     }
 }
 
-// Плагин для работы с JPA-аннотациями
 allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
