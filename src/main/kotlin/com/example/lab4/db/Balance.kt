3c000e3ed7e66b4523549badf13e7fa6d85d272e
Calmegi <116198893+Calmegi@users.noreply.github.com>
2025-03-17 13:47:26 +0300

    Add files via upload

--- src/main/kotlin/com/example/lab4/db/Balance.kt
+++ src/main/kotlin/com/example/lab4/db/Balance.kt
@@ -3,19 +3,14 @@ package com.example.lab4.db
 import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.*
 
-/*
-Сущность “Balance” представляет баланс пользователя.
-Каждая запись связывает определённую валюту с количеством (в минимальных единицах)
-и принадлежит пользователю (ExchangerUser).
-*/
 
 @Entity
 @Table(uniqueConstraints = [UniqueConstraint(columnNames = ["user_id", "currencyCode"])])
 data class Balance(
     @Column(nullable = false)
-    var currencyCode: String, // Например, "RUB", "USD"
+    var currencyCode: String, 
     @Column(nullable = false)
-    var amount: Int, // Сумма в минимальных единицах (копейки)
+    var amount: Int, 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     @JsonIgnore
@@ -26,7 +21,6 @@ data class Balance(
     var id: Long? = null
 
     init {
-        // Приведение к верхнему регистру для единообразия.
         this.currencyCode = currencyCode.uppercase()
     }
 }
